d51841091c68973903e9d56e91ced3cdaa881247
ericchagnon15 <eric.chagnon15@gmail.com>
2023-10-23 11:44:37 -0700

    remove unused import

--- berteley/models.py
+++ berteley/models.py
@@ -4,7 +4,6 @@ import nltk
 import pandas as pd
 from bertopic import BERTopic
 from gensim.models.coherencemodel import CoherenceModel
-from octis.evaluation_metrics.coherence_metrics import Coherence
 from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 from sentence_transformers import SentenceTransformer, models
 from typing import List, Dict, Tuple, Union, Literal
